fa6dae02084fa79e18b4e27568ae0340bc7b9c6e
jagdishjangid80 <jagdishjangid8079@gmail.com>
2025-03-28 15:49:18 +0530

    updates all code

--- src/components/Home/PopsComponent.jsx
+++ src/components/Home/PopsComponent.jsx
@@ -5,25 +5,23 @@ const PopsComponent = ({ showPops }) => {
   if (!showPops) return null; 
 
   return (
-    <>
-      <motion.div
-        className="absolute inset-0 bg-black flex justify-center items-center z-50 backdrop-blur-xl"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+    <motion.div
+      className="absolute inset-0 bg-black flex justify-center items-center z-50 backdrop-blur-xl"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h1
+        className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold text-center tracking-wide leading-tight"
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1.2, opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
       >
-        <motion.h1
-          className="text-white sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center tracking-wide leading-tight"
-          initial={{ scale: 0, opacity: 0 }}
-          animate={{ scale: 1.2, opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-        >
-          Discover the future <br /> of housewares
-        </motion.h1>
-      </motion.div>
-    </>
+        Discover the future <br /> of housewares
+      </motion.h1>
+    </motion.div>
   );
 };
 
